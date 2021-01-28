e8ad733adaf459d5b380a340df5a6ca2ae65c24f
coderj001 <amirajubolchi001@gmail.com>
2021-01-28 21:07:58 +0530

    config currency view and templates

--- MoneyTracker/userpreferences/views.py
+++ MoneyTracker/userpreferences/views.py
@@ -8,28 +8,47 @@ from userpreferences.models import Userpreference
 
 
 class GeneralPrefView(View):
-    def get(self, request):
+    def currency_data(self):
         currency_data = list()
-
         full_path = settings.BASE_DIR.parent / 'currencies.json'
         with open(full_path, "r") as f:
             data = json.load(f)
             for k, v in data.items():
                 currency_data.append({'name': k, 'value': v})
+        return currency_data
 
+    def currency_pref(self, user):
+        user_preferences = Userpreference.objects.get_or_create(
+            user=user
+        )
+        return user_preferences[0].currency
+
+    def get(self, request):
         return render(
             request,
             'preference/index.html',
-            context={'currencies': currency_data}
+            context={
+                'currencies': self.currency_data(),
+                'current_currency': self.currency_pref(request.user)
+            }
         )
 
     def post(self, request):
         currency = request.POST.get('currency')
-        user_preferences = Userpreference.objects.get(user=request.user)
-        user_preferences.currency = currency
-        user_preferences.save()
-        messages.add_messages(
+        user_preferences = Userpreference.objects.get_or_create(
+            user=request.user)
+        user_preferences[0].currency = currency
+        user_preferences[0].save()
+        messages.add_message(
             request,
             messages.SUCCESS,
             f"{request.user.username}, preference is saved."
+        )
+        return render(
+            request,
+            'preference/index.html',
+            context={
+                'currencies': self.currency_data(),
+                'current_currency': currency
+            }
         )
