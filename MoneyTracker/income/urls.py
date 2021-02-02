0dbbf248bf786f4ea64ef85e02b1f2b142eab014
coderj001 <amirajubolchi001@gmail.com>
2021-02-03 02:13:46 +0530

    created seach for income view

--- MoneyTracker/income/urls.py
+++ MoneyTracker/income/urls.py
@@ -1,8 +1,13 @@
-from income.views import (home_income,)
+from income.views import (home_income, add_income,
+                          edit_income, delete_income, search_income)
 from django.urls import path
 
 app_name = 'income'
 
 urlpatterns = [
     path('', home_income, name="home-income"),
+    path('add_income/', add_income, name="add-income"),
+    path('search_income/', search_income, name="search-income"),
+    path('edit_income/<int:id>/', edit_income, name="edit-income"),
+    path('delete_income/<int:id>/', delete_income, name="delete-income"),
 ]
--- MoneyTracker/income/views.py
+++ MoneyTracker/income/views.py
@@ -1,9 +1,16 @@
+import json
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
-from income.models import Income, Source
 from django.core.paginator import Paginator
+from django.db.models import Q
+from django.http import JsonResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.utils.timezone import now
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from income.models import Income, Source
 
 
 @login_required(login_url=reverse_lazy('auth:login'))
@@ -40,8 +47,65 @@ def add_income(request):
                               source=source_res, date=date)
         messages.add_message(request, messages.SUCCESS,
                              "Income saved successfully.")
-        return redirect('expenses:home-income')
+        return redirect('income:home-income')
 
     return render(request, 'income/add_income.html',
                   context={'sources': source}
                   )
+
+
+@login_required(login_url=reverse_lazy('auth:login'))
+def edit_income(request, id):
+    income = Income.objects.get(pk=id)
+    source = Source.objects.all()
+    if request.method == 'GET':
+        return render(request, 'income/edit_income.html', context={
+            'fieldVal': income,
+            'sources': source,
+        })
+
+    if request.method == 'POST':
+        amount = request.POST.get('amount')
+        if not amount:
+            messages.add_message(request, messages.WARNING,
+                                 "Amount is required.")
+            return redirect('income:edit-income', id=income.pk)
+
+        description = request.POST.get('description')
+        source_res = request.POST.get('source')
+        date = request.POST.get('date')
+        if not date:
+            date = now().date()
+        income.amount = amount
+        income.description = description
+        income.source = source_res
+        income.date = date
+        income.save()
+        messages.add_message(request, messages.SUCCESS,
+                             "Income is updated successfully.")
+        return redirect('income:edit-income', id=income.pk)
+
+
+@login_required(login_url=reverse_lazy('auth:login'))
+def delete_income(request, id):
+    income = Income.objects.get(pk=id)
+    income.delete()
+    return redirect('income:home-income')
+
+
+@csrf_exempt
+@require_POST
+@login_required
+def search_income(request):
+    if request.method == 'POST':
+        search_var = json.loads(request.body).get('searchText')
+        income = Income.objects.filter(
+            Q(amount__startswith=search_var) |
+            Q(date__startswith=search_var) |
+            Q(description__icontains=search_var) |
+            Q(source__icontains=search_var),
+            owner=request.user)[:6]
+
+        data = expenses.values()
+
+        return JsonResponse(list(data), safe=False)
