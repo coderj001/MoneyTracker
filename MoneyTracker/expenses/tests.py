5faf15e7f1d1811f897be4bb782418ae307748b8
coderj001 <amirajubolchi001@gmail.com>
2021-03-11 21:24:49 +0530

    added expenses models test

--- MoneyTracker/expenses/tests.py
+++ MoneyTracker/expenses/tests.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from expenses.models import Expense, Catagory
 
-# Create your tests here.
+user = User.objects.get(username='admintest')
+
+
+class ExpenseTestCase(TestCase):
+    def setUp(self):
+        self.expense = Expense.objects.create(
+            amount=1000,
+            description='test case',
+            owner=user,
+            category='BUSSINESS'
+        )
+
+    def test_expense_model(self):
+        d = self.expense
+        self.assertTrue(isinstance(d, Expense))
+
+
+class CatagoryTestCase(TestCase):
+    def setUp(self):
+        self.category = Catagory.objects.create(
+            name='TestCase'
+        )
+
+    def test_catagory_model(self):
+        d = self.category
+        self.assertTrue(isinstance(d, Catagory))
+        self.assertEqual(str(d), 'TestCase')
