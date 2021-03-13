3d99658d00c4327ca6897646e6d16309b94ee49c
coderj001 <amirajubolchi001@gmail.com>
2021-03-13 18:28:19 +0530

    heroku config update wsgi.py file changes

--- MoneyTracker/MoneyTracker/wsgi.py
+++ MoneyTracker/MoneyTracker/wsgi.py
@@ -3,6 +3,6 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MoneyTracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MoneyTracker.settings.dev')
 
 application = Cling(get_wsgi_application())
